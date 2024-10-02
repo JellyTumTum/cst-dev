b386db391a24274e75a1104d3642ff2094d423b2
JellyTumTum <cameronst12345@gmail.com>
2024-10-02 16:52:21 +0100

    Added Contact Section to Welcome page

--- frontend/src/components/pages/Welcome.js
+++ frontend/src/components/pages/Welcome.js
@@ -12,7 +12,7 @@ const Welcome = ({ openTabs, selectedTab, onLinkClick, tabPointer, setTabPointer
             <Typography variant='paragraph' className={`text-textSecondary text-lg mb-[3.5rem] text-center ${isExplorerOpen ? 'md:text-left' : 'sm:text-left'} `}>Cameron Thomas, 21</Typography>
             <div className={`flex flex-col gap-4 ${isExplorerOpen ? 'md:flex-row' : 'sm:flex-row' } `}>
                 <div className={`h-full w-full  flex items-center flex-col ${isExplorerOpen ? 'md:w-1/2 md:items-start' : 'sm:w-1/2 sm:items-start'} `}>
-                    <Typography className="text-textMain text-lg">Start</Typography>
+                    <Typography  variant={'h1'} className="text-textMain font-thin mb-2 text-xl">Start</Typography>
                     {pages.map((page, index) => (
                         <FileName
                             key={index}
@@ -27,9 +27,13 @@ const Welcome = ({ openTabs, selectedTab, onLinkClick, tabPointer, setTabPointer
 
                 </div>
                 <div className={`h-full w-full mt-5 ${isExplorerOpen ? 'md:w-1/2 md:mt-0' : 'sm :w-1/2 sm:mt-0'} `}>
-                    <Typography className={`text-textMain text-lg text-center ${isExplorerOpen ? 'md:text-left' : 'sm:text-left'} `}>Introduction</Typography>
-                    <Typography variant="paragraph" className={`text-textSecondary text-sm text-center ${isExplorerOpen ? 'md:text-left' : 'sm:text-left'} `}>Hi, I'm Cameron! I recently graduated with a BSc in Computer Science from the University of Birmingham and have a strong passion for software development. I'm currently looking for opportunities as a software developer where I can apply my skills and continue growing in the field. Feel free to explore my projects and get in touch!</Typography>
+                    <Typography variant={'h1'} className={`text-textMain font-thin mb-2 text-center text-xl ${isExplorerOpen ? 'md:text-left' : 'sm:text-left'} `}>Introduction</Typography>
+                    <Typography variant="paragraph" className={`text-textSecondary  text-sm md:text-lg text-center ${isExplorerOpen ? 'md:text-left' : 'sm:text-left'} `}>Hi, I'm Cameron! I recently graduated with a BSc in Computer Science from the University of Birmingham and have a strong passion for software development. I'm currently looking for opportunities as a software developer where I can apply my skills and continue growing in the field. Feel free to explore my projects and get in touch!</Typography>
                 </div>
+            </div>
+            <div className={`group relative flex justify-center items-center flex-row rounded-xl border-2 bg-explorerMain border-borderColor cursor-pointer shadow-lg group gap-4 p-2 px-2 mt-4`}>
+                <h1 className="text-textMain text-lg"> Contact Me: <strong>cameron@cst.dev</strong></h1>
+
             </div>
 
         </div>
